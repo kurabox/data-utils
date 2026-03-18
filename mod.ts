05ea360296381046aa9383096e1e1b0a5ef555af
thinh-tr <ducthinh2049@gmail.com>
2026-03-18 10:51:19 +0700

    refactor: chuyển mod.ts ra root folder

---
+++ mod.ts
@@ -0,0 +1,3 @@
+export * as language from "./src/support-languages.ts"; // các thành phần của support-languages
+export * as helperfuncs from "./src/helper-funcs.ts";   // các thành phần của helper-funcs
+export * as searchdatamodels from "./src/search-data-models.ts";    // các thành phần của search-data-models
